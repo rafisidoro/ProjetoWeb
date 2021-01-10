b67403f70e126d2c51dae97323a3aba7458e5554
Maria Morais <75227121+moraisabella@users.noreply.github.com>
2021-01-10 16:43:06 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Login</title>
+         <link rel="stylesheet" href="layout1.css"> 
+        <link href="https://fonts.googleapis.com/css?family=Cinzel&display=swap" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+    </head>
+    <body>
+        <div id="login1">
+        <h3> Login do usuario </h3>
+        </div>
+        <div id="login2">
+        <form> 
+        <label for="email"> E-mail </label>            
+        <input type='email' name="email" > <br>
+                         
+        <label for="Senha"> Senha </label>        
+        <input type='password' name="Senha" > 
+            
+        <div id="botao">
+             <button id="loginb" type="submit"> Entrar </button>
+        </div>
+       
+                </form> 
+        </div>
+        
+        <div id="linkcad">
+        <h2>Ainda não é cadastrado? <a href="cadastro.html"> Cadastre-se agora</a></h2>
+        </div>
+        
+         <a class="volt" href="index.html" > Voltar a página inicial</a>
+        
+        <div id="rodape">
+            <img id="pag" src="rodape.jpeg" alt="pagamentos">
+            <img id="redes" src="redes.jpeg" alt="redessociais">
+            <div id="linkr">
+            <table>
+                <tr>
+                <li> <a href="sobrealoja.html"> Sobre nós </a></li>
+                <li> <a href="cadastroproduto2.html"> Página do administrador </a></li>
+                </tr>
+            </table>
+            </div>
+        </div>
+    </body>
+</html>
